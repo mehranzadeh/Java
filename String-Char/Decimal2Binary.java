a437dc50093907d22b1f869e09eb2b10ffbb4d9f
Amin Mehranzadeh <mehranzadeh@gmail.com>
2017-05-01 18:56:51 +0430

    Add files via upload

---
+++ String-Char/Decimal2Binary.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalesson;
+import java.lang.Math;
+/**
+ *
+ * @author mehran
+ */
+public class Decimal2Binary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int num=15;
+      String s="";
+      int remain=0;
+      
+      while (num>=2){
+          remain = Double.valueOf(num %2).intValue(); 
+          s= s + Integer.valueOf(remain).toString();
+          num = Double.valueOf(num /2).intValue(); 
+      }  
+          s=Integer.valueOf(num).toString()+ s;     
+          System.out.println(s);
+
+    }
+    
+  
+}
---
+++ String-Char/charArray2String.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalesson;
+
+/**
+ *
+ * @author mehran
+ */
+public class charArray2String {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char[] charArray= {'b','o','o','k'};
+        String str = new String (charArray);   
+        System.out.println(str);
+       
+    }
+    
+  
+}
